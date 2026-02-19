296d6305da2a099fd6e80770ad9d7ba6bff0d750
Aman Kumar <ar1075840@gmail.com>
2026-02-19 18:01:43 +0530

    Updated lenis package

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Lenis from '@studio-freight/lenis'
+// import Lenis from '@studio-freight/lenis'
+import Lenis from 'lenis'
+
 
 // Create Lenis instance
 const lenis = new Lenis({
